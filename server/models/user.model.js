835ff94e1c7b780cfb7a685b43f539136aa08a09
smals-andme <andra.mertilos@smals.be>
2024-05-22 22:33:30 +0200

    Created the user registration endpoint

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,50 +1,53 @@
 const { PrismaClient }=require('@prisma/client')
 const bcrypt = require('bcrypt');
 
-const prisma = new PrismaClient({log: ['query', 'info', 'warn', 'error'],}
+const prisma = new PrismaClient({log: ['query', 'info', 'warn', 'error'],  errorFormat: "minimal",}
 )
 
 async function createUser(username, password,role){
-    //Check required properties
-    if (!username || !password){
+         //Check required properties
+    if (!username || !password || !role){
         return ({
            error: 'Missing required properties.'
         })
     }
     //Check if username doesn't exist already
-    //   const userExists = await existsUser(username)
-    //   if (userExists) {
-    //     return ({error: "Username already exists."})
-    //   }
+   //This check is done by enforcing a unique constraint on username(email) in the DB, done at the time of user creation
 
     //Create new user
     //Hash password 
-    bcrypt.hash(password, 10, (err, hash) => {
-        if (err) {
-            // Handle error
-            return ({error: 'There was an error with your request.Try again later.'})
-        }
-        //Save user details to database
-        prisma.user.create({
-            data: {
-                email: username,
-                password: hash,
-                role: role,
-                tokenCreationTimestamp: new Date().toISOString()
-            }
-           
-          }).then(data=> {
-            return ({message: "Registration successful. User created with id: "+ data.id})
-        }).catch(err=> {
-            return ({error: err})
-        })
-       //console.log(userRegistration)
-    
-    
-})
-   
+    const hash = bcrypt.hashSync(password, 10);
+    //Save user details to DB
+    if (hash) {
+        try {
+            const userRegistration= await  prisma.user.create({
+                data: {
+                    email: username,
+                    password: hash,
+                    role: role,
+                    token: 'This should be nullable',
+                    tokenCreationTimestamp: new Date().toISOString()
+                }
+               
+              })
+              return ({message: "Registration successful. User has been created with id: " + userRegistration.id})
 
-}
+        }
+        catch(err){
+            if (err.name==="PrismaClientValidationError"){
+                return ({error:"Provided input parameters or type of input parameters is invalid." })
+            } else if (err.name==="PrismaClientKnownRequestError"){
+                return ({error: "Username already exists."})
+            }
+            return({error:err.name})
+        }
+
+    }
+
+
+    } 
+  
+
 
 module.exports= {
     createUser
